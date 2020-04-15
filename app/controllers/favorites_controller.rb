bfa03a080792f19d2fced66346586ef6e7534021
j.webapp <j.webprogram@gmail.com>
2020-04-15 14:46:21 +0000

    favorite ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,16 @@ class FavoritesController < ApplicationController
    before_action :authenticate_user!, only: [:create,:destroy]
   
   def create
-    post = Post.find(params[:post_id])
-    current_user.like(post)
-    flash[:success] = 'お気に入りに登録しました。'
-    redirect_back(fallback_location: root_path)
+    @post = Post.find(params[:post_id])
+    current_user.like(@post)
+    # flash[:success] = 'お気に入りに登録しました。'
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    current_user.unlike(post)
-    flash[:success] = 'お気に入りを解除しました。'
-    redirect_back(fallback_location: root_path)
+    @post = Post.find(params[:post_id])
+    current_user.unlike(@post)
+    # flash[:success] = 'お気に入りを解除しました。'
+    # redirect_back(fallback_location: root_path)
   end
 end
